9245c2f7c1e7609b481c086052c233a22ed1178d
anggaraswn <anggara11swn@gmail.com>
2024-03-27 19:25:38 +0700

    Initial Commit

---
+++ SICON/SICONApp.swift
@@ -0,0 +1,17 @@
+//
+//  SICONApp.swift
+//  SICON
+//
+//  Created by Anggara Satya Wimala Nelwan on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SICONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
