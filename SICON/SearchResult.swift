d4f87c008634c1b80a8a52f57864e64718d4d1b9
anggaraswn <anggara11swn@gmail.com>
2024-04-01 16:43:30 +0700

    Search Result Page

---
+++ SICON/SearchResult.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResult.swift
+//  SICON
+//
+//  Created by Anggara Satya Wimala Nelwan on 30/03/24.
+//
+
+import SwiftUI
+
+struct SearchResult: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchResult()
+}
---
+++ SICON/VehicleNumber.swift
@@ -0,0 +1,8 @@
+//
+//  VehicleNumber.swift
+//  SICON
+//
+//  Created by Anggara Satya Wimala Nelwan on 28/03/24.
+//
+
+import Foundation
